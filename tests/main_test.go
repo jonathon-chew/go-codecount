22b330ea2203f6f817dfa92d837e814ca47e90b1
Jonathon Chew <jonchew626@hotmail.com>
2026-03-15 21:03:11 +0000

    update: replace the hard coded  path with a local one

--- tests/main_test.go
+++ tests/main_test.go
@@ -2,12 +2,22 @@ package main_test
 
 import (
 	"os"
+	"path/filepath"
+	"runtime"
 	"testing"
 )
 
 func Benchmark_Speed(b *testing.B) {
 
-	os.Chdir("/Users/hunteradder626/Documents/Scripts")
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		b.Fatal("unable to determine test file location")
+	}
+
+	repoRoot := filepath.Clean(filepath.Join(filepath.Dir(filename), "..", "..", "..", ".."))
+	if err := os.Chdir(repoRoot); err != nil {
+		b.Fatalf("unable to change directory to repo root: %v", err)
+	}
 
 	os.Args = append(os.Args, "-i", ".git", "venv")
 
